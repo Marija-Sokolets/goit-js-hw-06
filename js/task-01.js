4eb4be15dfaaebfe62d06432860b18819bc3ef33
Marija-Sokolets <128020732+Marija-Sokolets@users.noreply.github.com>
2023-07-15 12:37:51 +0300

    task-01

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const categoriesUl = document.querySelectorAll('.item');
+
+console.log(`Number of categories: ${categoriesUl.length}`);
+
+categoriesUl.forEach((el) => {
+  let title = el.querySelector("h2");
+  let titleLi = el.querySelectorAll("li");
+  console.log(`Category: ${title.textContent}`);
+  console.log(`Elements: ${titleLi.length}`);
+});
