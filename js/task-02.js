264d60528048a64d4ee514918034b54b9c87fbe2
Marija-Sokolets <128020732+Marija-Sokolets@users.noreply.github.com>
2023-07-15 13:03:24 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ulEl = document.createElement("ul");
+for (const ingredient of ingredients) {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  liEl.classList.add('item')
+  ulEl.appendChild(liEl);
+}
+console.log(ulEl);
+document.body.appendChild(ulEl);
